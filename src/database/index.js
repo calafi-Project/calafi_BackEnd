72782ad5403690fd6cd8abea1012d32e4f71a188
soul071213 <js071213asd@gmail.com>
2025-06-04 11:30:03 +0900

    df

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,41 @@
-const { query } = require('express');
+// const { query } = require('express');
+// const mysql = require('mysql2/promise');
+
+// exports.pool=mysql.createPool(
+//     {
+//         host:process.env.DB_HOST,
+//         user:process.env.DB_USERNAME,
+//         password:process.env.DB_PASSWORD,
+//         database:process.env.DB_DATABASE,
+//         port:process.env.DB_PORT,
+//         waitForConnections:true,
+//         connectionLimit:10,
+//         queueLimit:0
+//     }
+// );
+
+
+// exports.pool.query = async (queryString,params)=>{
+//     const [result] = await this.pool.execute(queryString,params);
+//     return result;
+// }
+
 const mysql = require('mysql2/promise');
 
-exports.pool=mysql.createPool(
-    {
-        host:process.env.DB_HOST,
-        user:process.env.DB_USERNAME,
-        password:process.env.DB_PASSWORD,
-        database:process.env.DB_DATABASE,
-        port:process.env.DB_PORT,
-        waitForConnections:true,
-        connectionLimit:10,
-        queueLimit:0
-    }
-);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
+exports.pool = pool;
 
-exports.pool.query = async (queryString,params)=>{
-    const [result] = await this.pool.execute(queryString,params);
-    return result;
-}
+exports.query = async (queryString, params) => {
+  const [result] = await pool.execute(queryString, params);
+  return result;
+};
